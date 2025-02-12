e97ca090265e3b4fcafd943aed046c6bc98c626d
Dylan Michael Jones <dylanmj@tlu.ee>
2025-02-12 19:32:53 +0000

    lisasin kommentaari

--- 01_funktsioon/kodutöö1.ts
+++ 01_funktsioon/kodutöö1.ts
@@ -1,6 +1,7 @@
 let noodid = ["C", "C#", "D", "D#", "E", "F","F#","G", "G#", "A", "A#","B"];
 let keelte_noodid = {1:4, 2:9, 3:2, 4:7, 5:11};
 
+//Funktsioon leiab keele ning krihvi järgi noodi nimetuse kitarrikaelal
 function noodileidmine(keel:number, krihv:number):string {
     let leitav_noot:number = (keelte_noodid[keel] + krihv) % 12;
     let noodinimi:string = noodid[leitav_noot];
